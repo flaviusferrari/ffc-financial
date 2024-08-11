17d88482e2e67875cd4bf97d1d84f781894a5a89
flaviusferrari <flaviusferrari@gmail.com>
2024-08-11 11:24:08 -0300

    Inserido o CSS do AdminLte

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,10 +3,14 @@
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
-    @vite('resources/js/app.js')
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">    
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     @inertiaHead
   </head>
   <body>
-    @inertia
+    <div class="wrapper">
+      @inertia
+    </div>
   </body>
 </html>
