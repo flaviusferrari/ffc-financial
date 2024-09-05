516d9bd52df7b4735e680b20843323f417307422
flaviusferrari <flaviusferrari@gmail.com>
2024-09-05 10:58:54 -0300

    Término do Layout básico

--- resources/js/Pages/Layout.vue
+++ resources/js/Pages/Layout.vue
@@ -1,18 +1,22 @@
 <template>
-  <main>
-    <header>
-        <NavBar />
-    </header>
-    <article>
-      <slot />
-    </article>
-  </main>
+  <NavBar />
+  <SideBar />
+    <div class="content-wrapper">
+      <!-- Content Header (Page header) -->
+      <div class="content-header">
+        <div class="container-fluid">
+          <slot />
+        </div>
+      </div>
+    </div>
+  <Footer />
 </template>
 
 <script setup>
-import NavBar from '../Components/NavBar.vue'
+import NavBar from "../Components/NavBar.vue";
+import SideBar from "../Components/Sidebar.vue";
+import Footer from "../Components/Footer.vue";
 </script>
 
 <style>
-
 </style>
