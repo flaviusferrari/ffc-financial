c941c778188ea16e8aaf0790060d427a70ac6334
flaviusferrari <flaviusferrari@gmail.com>
2024-09-30 07:40:03 -0300

    Ajustando o Layout no Dashboard

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,11 +4,13 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <title inertia>{{ config('app.name', 'FFC Financeiro') }}</title>
 
         <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+        <!-- Font Awesome -->
+        <link rel="stylesheet" href="{{ asset('fontawesome/css/all.css') }}">
+        <link rel="stylesheet" href="{{ asset('adminlte.min.css') }}">
 
         <!-- Scripts -->
         @routes
