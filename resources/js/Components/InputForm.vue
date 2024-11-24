e0fc54f445168a0fde56a7d146af291f0f474853
flaviusferrari <flaviusferrari@gmail.com>
2024-11-24 09:51:55 -0300

    Componente InputForm

---
+++ resources/js/Components/InputForm.vue
@@ -0,0 +1,27 @@
+<script setup>
+    defineProps({
+        label: {
+            type: String,
+            required: true,
+        },
+        name: {
+            type: String
+        },
+        id: {
+            type: String
+        },
+        value: {
+            type: String
+        },
+        placeholder: {
+            type: String
+        }
+    });
+</script>
+
+<template>
+    <div class="form-group">
+      <label>{{ label }}</label>
+      <input type="text" class="form-control" :id="id" :name="name" :placeholder="placeholder" :value="value" />
+    </div>
+</template>
